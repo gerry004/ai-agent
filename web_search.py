401fb5ef59fc8747def4505b05d62ae62d5bdf06
Gerry Yang <gerry04y@gmail.com>
2025-01-19 21:51:42 +0000

    Initial Commit

---
+++ web_search.py
@@ -0,0 +1,16 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.tools.duckduckgo import DuckDuckGo
+
+from dotenv import load_dotenv
+load_dotenv()
+
+web_agent = Agent(
+    name="Web Agent",
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+web_agent.print_response("Tell me about the tiktok ban?", stream=True)
